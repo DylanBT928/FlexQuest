a7a231b07d415ec7b5e5d7c51c371ab1530619e0
Dylan Tran <dylanbt928@gmail.com>
2025-01-25 18:52:18 -0800

    added icons fr navbar

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Image } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import WorkoutScreen from './screens/WorkoutScreen';
 import CaloriesScreen from './screens/CaloriesScreen';
@@ -16,12 +16,55 @@ import { UserProvider } from './Contexts/Usercontext';
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+const homeIcon = require('../assets/images/home.png'); // Import the home.png image
+const caloriesIcon = require('../assets/images/calories.png'); // Import the calories.png image
+const workoutIcon = require('../assets/images/workout.png'); // Import the workout.png image
+
 // Create the Tab Navigator
 const TabNavigator = () => (
-  <Tab.Navigator initialRouteName="Home">
-    <Tab.Screen name="Workout" component={WorkoutScreen} />
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Calories" component={CaloriesScreen} />
+  <Tab.Navigator
+    initialRouteName="Home"
+    screenOptions={{
+      tabBarShowLabel: false, // Hide the text labels
+      tabBarStyle: { height: 80 }, // Increase the height of the tab bar
+    }}
+  >
+    <Tab.Screen
+      name="Workout"
+      component={WorkoutScreen}
+      options={{
+        tabBarIcon: ({ size }) => (
+          <Image
+            source={workoutIcon}
+            style={{ width: 110, height: 110, marginTop: 25, marginLeft: 10 }} // Make the icon bigger
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: ({ size }) => (
+          <Image
+            source={homeIcon}
+            style={{ width: 120, height: 120, marginTop: 42 }} // Make the icon bigger
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Calories"
+      component={CaloriesScreen}
+      options={{
+        tabBarIcon: ({ size }) => (
+          <Image
+            source={caloriesIcon}
+            style={{ width: 72, height: 72, marginTop: 40}} // Make the icon bigger
+          />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
@@ -80,12 +123,3 @@ const App = () => {
 };
 
 export default App;
-
-// Safe styles for reusable layout
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
